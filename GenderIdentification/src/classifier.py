be4cb8d2ee52d57ac74242bfba0f64c3515c838d
jkdll <dalli.jake.587@gmail.com>
2015-02-20 02:05:40 +0100

    Tajjeb ax Tajjeb, Tajjeb ghax jahdem.

--- GenderIdentification/src/classifier.py
+++ GenderIdentification/src/classifier.py
@@ -3,12 +3,42 @@ import pprint
 import random
 import nltk
 
+# Function to Open the CSV
 def loadCsv(filename):
     lines = csv.reader(open(filename, "rt"))
     sds = list(lines)
     return sds
 
-dataDir = 'C:\\Users\\Jake\\Desktop\\genID\\data152.csv'
+# Function for K-fold Validation
+# 
+def kfoldCrossValidation(docs,k,classifierType):
+    random.shuffle(docs) # Shuffle the instances
+    print('>> Starting ' + str(k) + '-fold Validation for ' + classifierType);
+    kLimit = int(round(len(docs)/k,0))
+    # print('K Limit: ' + str(kLimit))
+    accuracy = 0;
+    for i in range(0,int(k)):
+        lowerBound = i*kLimit
+        upperBound = ((i+1)*kLimit)
+        # print('Bounds : ' + str(lowerBound) + ',' + str(upperBound))
+        tSet = []
+        vSet = []
+        for j in range(0,int(len(docs))):
+            if( j < lowerBound or j >= upperBound):
+                tSet.append(docs[j])
+            else:
+                vSet.append(docs[j])
+        # print('>> Training Set Size: ' + str(len(tSet)))
+        # print('>> Validation Set Size: ' + str(len(vSet)))
+        classifier = nltk.NaiveBayesClassifier.train(tSet)
+        tempAccuracy = nltk.classify.accuracy(classifier, validationSet)
+        print ('>> Fold ' + str(i) + ' -> Training Set Size: ' + str(len(tSet)) + ', Validation Set Size: ' + str(len(vSet)) 
+               + ' Accuracy: ' + str(tempAccuracy))
+        accuracy = accuracy + tempAccuracy
+    accuracy = accuracy/k
+    print('>> Total Accuracy: ' + str(accuracy))
+    
+dataDir = 'C:\\Users\\Jake\\Desktop\\genID\\biggerdata.csv'
 sds = loadCsv(dataDir)
 
 # Remove Headers, and shuffle data set.
@@ -16,7 +46,7 @@ headers = sds[0]
 sds.pop(0)
 print(sds[0])
 random.shuffle(sds)
-
+    
 # The List used for the Data Set
 dataSet = []
 
@@ -36,9 +66,7 @@ for d in sds:
     dataSet.append([featureIQ,str(d[10])])
 
 # Calculate Split Ratio and Split Limits (TODO: Amend for Cross-Validation)
-p = len(dataSet)/20
-p = int(round(p,0))
-limit = int(p)
+limit = int(round(len(dataSet)/10,0))
 
 # Split into Training Set and Validation Set (TODO: Amend for Cross-Validation)
 validationSet = dataSet[0:limit]
@@ -50,6 +78,11 @@ print('Data Set Length: ' + str(len(dataSet)))
 print('Validation Length: ' + str(len(validationSet)))
 print('Training Length: ' + str(len(trainingSet)))
 
-classifier = nltk.NaiveBayesClassifier.train(trainingSet)
-print(nltk.classify.accuracy(classifier, validationSet))
+print ('---------------------------')
 
+# kfoldCrossValidation(dataSet,int(10),'Naive Bayes')
+# classifier = nltk.NaiveBayesClassifier.train(trainingSet)
+# classifier = nltk.svm.train(trainingSet)
+# print(nltk.classify.accuracy(classifier, validationSet))
+
+    
